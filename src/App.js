1729ff13daf98cf0e4b68d97c0b74a8487473f7c
mitsurubil <mitsurubil@hotmail.com>
2020-03-15 18:36:27 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import logo from './cat.svg';
 import './App.css';
 
 function App() {
+
+  const [fact, setFact] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [getFact, setGetFact] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const result = await axios("https://cors-anywhere.herokuapp.com/https://cat-fact.herokuapp.com/facts/random")
+      // console.log(result.data);
+      setFact(`${result.data.text}`)
+      setLoading(false);
+    }
+    fetchData();
+  }, [getFact])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Random Cat Fact</h1>
+        {loading ? <p>Fact is Loading...</p> : <div id="cat-fact-container"><p id="cat-fact">
+          {fact}
+        </p></div>}
+        <button id="get-fact" onClick={() => setGetFact(!getFact)}>Get a new fact !</button>
       </header>
     </div>
   );
